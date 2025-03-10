bc600fc55dfa2e97591d013babee251499abdc24
Student <student@Administrators-Mac-mini.local>
2025-03-10 12:36:33 +0200

    Create Splash screen

---
+++ app/components/Button/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#4F63AC",
+        paddingVertical: 20,
+        paddingHorizontal: 8,
+        borderRadius: 8,
+        width: "100%"
+    },
+    title: {
+        fontSize: 16,
+        color: "#FFFFFF",
+        fontWeight: 700,
+        textAlign: "center",
+        fontWeight: "bold",
+    }
+})
+
+export default styles;
--- app/screens/auth/Splash/index.js
+++ app/screens/auth/Splash/index.js
@@ -1,14 +1,23 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Pressable } from "react-native";
 import React from "react";
 import { styles } from "./styles";
+import Button from "../../../components/Button";
 
 const SplashScreen = () => {
   return (
     <View style={styles.container}>
       <Image resizeMode="contain" style={styles.image} source={require('../../../../assets/images/splash_image.png')} />
-      <Text style={styles.title}>You'll Find</Text>
-      <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
-      <Text style={styles.title}>Here!</Text>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>You'll Find</Text>
+        <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
+        <Text style={styles.title}>Here!</Text>
+      </View>
+
+      <Button title="Sign Up" />
+      <Pressable onPress={() => console.log("Sign In")}>
+        <Text style={styles.signin}>Sign In</Text>
+      </Pressable>
     </View>
   );
 }
